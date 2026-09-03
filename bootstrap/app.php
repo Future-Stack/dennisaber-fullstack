d6725a7d3e531b1a5f35a36ebf962430dfbb8d66
Md. Mahbubur Rahman <mahbubur@softvence.com>
2026-09-03 15:17:29 +0600

    assets issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'member' => \App\Http\Middleware\EnsureMember::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
